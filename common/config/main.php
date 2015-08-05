3b94de93f6e323c1a81c387e088109fd13574bdc
konst <gobeliak@rozetka.com.ua>
2015-08-05 13:03:14 +0300

    create project

---
+++ common/config/main.php
@@ -0,0 +1,20 @@
+<?php
+return [
+    'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
+    'components' => [
+        'cache' => [
+            'class' => 'yii\caching\FileCache',
+        ],
+        'mongodb' => [
+            'class' => '\yii\mongodb\Connection',
+            'dsn' => 'mongodb://localhost:27017/cards',
+        ],
+        'authManager' => [
+            'class' => 'yii\rbac\DbManager',
+            'defaultRoles' => ['guest'],
+        ],
+        'libs' => [
+            'class' => 'common\libs',
+        ],
+    ],
+];
